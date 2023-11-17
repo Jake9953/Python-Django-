65916d2bdb8ffaba3324aadcc7fe200f77e9c134
Jake9953 <jacobwambua793@gmail.com>
2023-11-17 04:56:49 +0300

    Simple maths operations with JS

---
+++ All Exercises and Projects used in learning/Javascript Exercises/fisrtexe.js
@@ -0,0 +1,6 @@
+var grams = prompt("Enter your weight in grams and we will convert it to grams: ")
+
+var kilograms = grams / 1000
+
+alert("Your weight in kilograms is " + kilograms + "kg")
+console.log("Conversion Completed")
