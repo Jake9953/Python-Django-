53bdf2e557d0e8e794a03f0eac444f93326be297
Jake9953 <jacobwambua793@gmail.com>
2023-12-25 07:09:11 +0300

    strings basics

--- BackEnd/Python/numbers.py
+++ BackEnd/Python/numbers.py
@@ -10,3 +10,23 @@ tax_rate = 60000
 my_taxes = my_income * tax_rate
 
 print(my_taxes)
+
+
+
+
+# strings
+# basics of strings
+
+'hello' #string
+"Hello" #string
+
+# Indexing strings
+mystring = "jacobwambua"
+
+print(mystring[0]) # To index a string we use the square brackets
+print(mystring[-1]) # To index a string we use the square brackets
+
+# slicing strings
+mystring = "wambua"
+
+print(mystring[2:]) #This will slice the string wambua, starting at index 2 until the last letter
