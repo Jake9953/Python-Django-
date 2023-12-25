c6d57b70da80f1b29351c8ea2e9ad3c6f6d3c48b
Jake9953 <jacobwambua793@gmail.com>
2023-12-25 05:19:52 +0300

    Done, but still needs some work to be done on it.

--- BOOSTRAP/bootstrap-5.1.3-dist/bootstrap-5.1.3-dist/game.js
+++ BOOSTRAP/bootstrap-5.1.3-dist/bootstrap-5.1.3-dist/game.js
@@ -11,6 +11,19 @@ function clearBoard() {
 }
 
 restartButton.addEventListener("click", clearBoard);
-// check the square marker
 
+// check the square marker
+function changeMarker() {
+  if (this.textContent === " ") {
+    this.textContent = "X";
+  } else if (this.textContent === "X") {
+    this.textContent = "O";
+  } else {
+    this.textContent = " ";
+  }
+}
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener("click", changeMarker)
+}
 // for loop to add event listeners to all the squares
