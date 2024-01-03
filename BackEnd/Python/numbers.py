9ba702933d6de46930620996c60d26629aad35c4
Jake9953 <jacobwambua793@gmail.com>
2024-01-03 22:32:19 +0300

    M

--- BackEnd/Python/numbers.py
+++ BackEnd/Python/numbers.py
@@ -1,42 +1,55 @@
-# Variable assignment
+# # Variable assignment
 
-a = 5
+# a = 5
 
-print(a)
+# print(a)
 
-my_income = 100000
-tax_rate = 60000
+# my_income = 100000
+# tax_rate = 60000
 
-my_taxes = my_income * tax_rate
+# my_taxes = my_income * tax_rate
 
-print(my_taxes)
+# print(my_taxes)
 
 
   
 
-# strings
-# basics of strings
+# # strings
+# # basics of strings
 
-'hello' #string
-"Hello" #string
+# 'hello' #string
+# "Hello" #string
 
-# Indexing strings
-mystring = "jacobwambua"
+# # Indexing strings
+# mystring = "jacobwambua"
 
-print(mystring[0]) # To index a string we use the square brackets
-print(mystring[-1]) # To index a string we use the square brackets
+# print(mystring[0]) # To index a string we use the square brackets
+# print(mystring[-1]) # To index a string we use the square brackets
 
-# slicing strings
-mystring = "wambua" 
+# # slicing strings
+# mystring = "wambua" 
 
-print(mystring[2:]) #This will slice the string wambua, starting at index 2 until the last letter
+# print(mystring[2:]) #This will slice the string wambua, starting at index 2 until the last letter
 
-# lists
-mylist = [1,2,3] 
+# # lists
+# mylist = [1,2,3] 
 
-# functions
-def addNUM(num1,num2):
-  return num1+num2
+# # functions
+# def addNUM(num1,num2):
+#   return num1+num2
 
-results = addNUM(2,3)
-print(results)
+# results = addNUM(2,3)
+# print(results)
+
+# OOP
+
+class person():
+  def __init__(self, name, course):
+    
+    self.name = name
+    self.course = course
+    
+me = person(name = "Jake" , course="ICT")
+
+print(me.name)
+print(me.course)
